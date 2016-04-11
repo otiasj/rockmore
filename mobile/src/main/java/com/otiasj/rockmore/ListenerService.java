70b09aacf108325a5e482cc42cd504dba733d36b
Julien Saito <jsaito@thehonestcompany.com>
2016-04-10 22:56:16 -0700

    Add receiver on application side

---
+++ mobile/src/main/java/com/otiasj/rockmore/ListenerService.java
@@ -0,0 +1,33 @@
+package com.otiasj.rockmore;
+
+import android.util.Log;
+
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.WearableListenerService;
+
+/**
+ * Created by juliensaito on 4/10/16.
+ */
+public class ListenerService extends WearableListenerService {
+    private static final String TAG = ListenerService.class.getCanonicalName();
+    private static final String WEARABLE_DATA_PATH = "/wearable_data";
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEvents) {
+        DataMap dataMap;
+        for (DataEvent event : dataEvents) {
+            // Check the data type
+            if (event.getType() == DataEvent.TYPE_CHANGED) {
+                // Check the data path
+                String path = event.getDataItem().getUri().getPath();
+                if (path.equals(WEARABLE_DATA_PATH)) {
+                    dataMap = DataMapItem.fromDataItem(event.getDataItem()).getDataMap();
+                    Log.v(TAG, "DataMap received from watch: " + dataMap);
+                }
+            }
+        }
+    }
+}
